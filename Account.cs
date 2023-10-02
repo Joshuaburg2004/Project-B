313eddad9166ecb675dc0e5e7ba432ba272fe0d5
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-10-02 15:49:19 +0200

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Quic;
+using System.Text;
+public class Account
+{
+    static int nextID;
+    public int id { get; private set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public Account(string name, string password)
+    {
+        id = Interlocked.Increment(ref nextID);
+        Name = name;
+        Password = password;
+    }
+    public static Account CreateAccount(string name, string password)
+    {
+        Account account = new Account(name, password);
+        AccountManager.Accounts.Add(account);
+        return account;
+    }
+    public static Account? GetAccount(int id)
+    {
+        foreach(Account account in AccountManager.Accounts)
+        {
+            if (account.id == id)
+            {
+                return account;
+            }
+        }
+        return null;
+    }
+    public static string? ChangePassword(string name, string password)
+    {
+        foreach (Account account in AccountManager.Accounts)
+        {
+            if(account.Name == name && account.Password == password)
+            {
+                Console.Write("Please enter your new password: ");
+                account.Password = Console.ReadLine() ?? password;
+                return account.Password;
+            }
+        }
+        return null;
+    }
+}
---
+++ AccountManager.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Net.Quic;
+using System.Text;
+public class AccountManager
+{
+    public static List<Account> Accounts = new();
+    public AccountManager()
+    {
+        string? FileCont = ControllerJson.ReadJson("Accounts.json");
+        if (FileCont != null)
+        {
+            Accounts = JsonConvert.DeserializeObject<List<Account>>(FileCont) ?? new List<Account> { };
+        }
+    }
+}
