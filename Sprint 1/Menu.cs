79b1f8db56f12d07150f4e8ebeaa72e8f15d9dc8
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-10-03 12:33:16 +0200

    Update Menu.cs

--- Sprint 1/Menu.cs
+++ Sprint 1/Menu.cs
@@ -3,22 +3,22 @@
 
 // category (fish/meat/vegan/vegetarian) 
 
-public class menu
+public class Menu
 {
-    public static List<menu> Menu_item = new();
+    public static List<Menu> Menu_item = new();
     public double Price;
     public string Category;
     public string Name;
-    public menu(string name, string category, double price)
+    public Menu(string name, string category, double price)
     {
         this.Price = price;
         this.Category = category;
         this.Name = name;
     }
-    public static List<menu> view_menu()
+    public static List<Menu> view_menu()
     {
-        menu item1 = new menu("Lahmacun","Meat",6.99);
-        menu item2 = new menu("Pizza pepperoni","Meat",12.5);
+        Menu item1 = new Menu("Lahmacun","Meat",6.99);
+        Menu item2 = new Menu("Pizza pepperoni","Meat",12.5);
         Menu_item.Add(item1);
         Menu_item.Add(item2);
         return Menu_item;
@@ -26,16 +26,9 @@ public class menu
 
     public static void view()
     {
-        foreach(menu item in Menu_item)
+        foreach(Menu item in Menu_item)
         {
             Console.WriteLine($"Name: {item.Name} Category: {item.Category} Price: {item.Price}");
         }
     } 
-}
-public class Program{
-    public static void Main()
-    {
-        menu.view_menu();
-        menu.view();
-    }
 }
