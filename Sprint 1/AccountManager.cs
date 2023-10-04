e0b5602e691c9a5fc019eb3d6ed433f1990b100b
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-10-04 10:37:23 +0200

    Update AccountManager.cs

--- Sprint 1/AccountManager.cs
+++ Sprint 1/AccountManager.cs
@@ -1,17 +1,19 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Data;
 using System.Net.Quic;
 using System.Text;
 public class AccountManager
 {
-    public static List<Account> Accounts = new();
+    public int Id {  get; set; }
+    public static List<IAccount> Accounts = new();
+    public static List<Customer> Customers = new();
     public AccountManager()
     {
         string? FileCont = ControllerJson.ReadJson("Accounts.json");
         if (FileCont != null)
         {
-            Accounts = JsonConvert.DeserializeObject<List<Account>>(FileCont) ?? new List<Account> { };
+            Accounts = JsonConvert.DeserializeObject<List<IAccount>>(FileCont) ?? new List<IAccount> { };
         }
     }
 }
