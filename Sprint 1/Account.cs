2c15fea421aa588d090e2410f0b693bb8aaa6a34
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-10-04 10:11:57 +0200

    Update Account.cs

--- Sprint 1/Account.cs
+++ Sprint 1/Account.cs
@@ -5,20 +5,24 @@ using System.Text;
 public class Account
 {
     static int nextID;
-    public int id { get; private set; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Password { get; set; }
-    public string Email { get; set; }
-    public Account(string email, string name, string password)
+    public string Email;
+    public string Status;
+
+    public Account(string name, string email, string password)
     {
-        id = Interlocked.Increment(ref nextID);
-        Email = email;
+        Id = Interlocked.Increment(ref nextID);
         Name = name;
         Password = password;
+        Email = email;
+        Status = "Customer";
     }
-    public static Account CreateAccount(string email, string name, string password)
+    public static Account CreateAccount(string name, string email, string password)
     {
-        Account account = new Account(email, name, password);
+
+        Account account = new Account(name, email, password);
         AccountManager.Accounts.Add(account);
         string json = JsonConvert.SerializeObject(AccountManager.Accounts, Formatting.Indented);
         JArray Object = JArray.Parse(json);
@@ -29,18 +33,18 @@ public class Account
     {
         foreach(Account account in AccountManager.Accounts)
         {
-            if (account.id == id)
+            if (account.Id == id)
             {
                 return account;
             }
         }
         return null;
     }
-    public static string? ChangePassword(string email, string name, string password)
+    public static string? ChangePassword(string name, string password)
     {
         foreach (Account account in AccountManager.Accounts)
         {
-            if(account.Email == email && account.Name == name && account.Password == password)
+            if(account.Name == name && account.Password == password)
             {
                 Console.Write("Please enter your new password: ");
                 account.Password = Console.ReadLine() ?? password;
