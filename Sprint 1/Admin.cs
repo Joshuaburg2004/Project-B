05e974bf737b10ea88e1b6292577ef789f92c7fc
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-10-04 10:54:39 +0200

    Add files via upload

---
+++ Sprint 1/Admin.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Net.Quic;
+using System.Text;
+public class Admin : Account
+{
+    
+    public Admin(string name, string email, string password, string role = "Admin") : base(name, email, password, role) { }
+    public static Admin CreateAdmin(string name, string email, string password)
+    {
+        Admin admin = new Admin(name, email, password);
+        AccountManager.Admins.Add(admin);
+        string json = JsonConvert.SerializeObject(AccountManager.Admins, Formatting.Indented);
+        JArray Object = JArray.Parse(json);
+        ControllerJson.WriteJson(Object, "Admins.json");
+        return admin;
+    }   
+}
