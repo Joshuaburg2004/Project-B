7e39dad37dc7f743efae8361c07ec0aa7f34c605
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-10-04 11:18:23 +0200

    Add files via upload

---
+++ Sprint 1/SuperAdmin.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Net.Quic;
+using System.Text;
+public class SuperAdmin : Admin
+{
+    public SuperAdmin(string name = "Gert-Jan den heijer", string email = "gjdenheijer@gmail.com", string password = "TestPassWord1234ForgorToChange", string role = "SuperAdmin") : base(name, email, password) { }
+    public Admin AddAdmin(string Name, string Email, string Password)
+    {
+        Admin admin = Admin.CreateAdmin(Name, Email, Password);
+        return admin;
+    }
+}
