ea5cc73f374ef00a6a91a2519defe54772992f27
Ber-kan <114362900+Ber-kan@users.noreply.github.com>
2023-10-09 14:41:19 +0200

    Create Program.cs

---
+++ Sprint 1/Program.cs
@@ -0,0 +1,28 @@
+public class Program
+{
+    public static void Main()
+    {
+        Table_6 table_1 = new Table_6();
+        Table_6 table_2 = new Table_6();
+
+        Table_4 table_3 = new Table_4();
+        Table_4 table_4 = new Table_4();
+        Table_4 table_5 = new Table_4();
+        Table_4 table_6 = new Table_4();
+        Table_4 table_7 = new Table_4();
+
+        Table_2 table_8 = new Table_2();
+        Table_2 table_9 = new Table_2();
+        Table_2 table_10 = new Table_2();
+        Table_2 table_11 = new Table_2();
+
+        Table_2 table_12 = new Table_2();
+        Table_2 table_13 = new Table_2();
+        Table_2 table_14 = new Table_2();
+        Table_2 table_15 = new Table_2();
+
+
+
+ 
+    }
+}
