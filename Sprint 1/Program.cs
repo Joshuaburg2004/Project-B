cfcf730a2f6c9d618d64841f740c5acbe19ac068
Ber-kan <114362900+Ber-kan@users.noreply.github.com>
2023-10-09 15:44:16 +0200

    Update Program.cs

--- Sprint 1/Program.cs
+++ Sprint 1/Program.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 public class Program
 {
     public static void Main()
@@ -21,8 +23,54 @@ public class Program
         Table_2 table_14 = new Table_2();
         Table_2 table_15 = new Table_2();
 
+        var Current_user = 0;
+/*--------------------------------------------------------------------------------------------------------------------------------*/
+        Console.ForegroundColor = ConsoleColor.Green;
+        RestaurantLayout.ViewLayout();
 
+        while (true)
+        {
+            Logo();
+            Console.WriteLine("""
+            press 1 to log in or to create an Account
+            press 2 to look at the menu
+            press 3 to read restaurant information
+            press 4 to close app
+            """);
+            int option = Convert.ToInt32(Console.ReadLine());
+            if (option == 1)
+            {
 
+            }
+            else if (option == 2)
+            {
+
+            } 
+            else if (option == 3)
+            {
+
+            }
+            else if (option == 4)
+            {
+                Environment.Exit(0);
+            }
+        }
  
     }
+    public static void Logo()
+    {
+        Console.WriteLine("""
+
+
+
+
+
+
+
+
+
+
+            """);
+    }
+
 }
