fcf32e134627f8503523180dc09a58de445c23ad
Ber-kan <114362900+Ber-kan@users.noreply.github.com>
2023-10-09 14:37:54 +0200

    Add files via upload

---
+++ Table_4.cs
@@ -0,0 +1,11 @@
+﻿public class Table_4 : Table
+{
+    private static int _nextId = 2;
+    public int TableNumber;
+
+    public Table_4() : base()
+    {
+        _nextId++;
+        TableNumber = _nextId;
+    }
+}
---
+++ Table_6.cs
@@ -0,0 +1,11 @@
+﻿public class Table_6 : Table
+{
+    private static int _nextId;
+    public int TableNumber;
+
+    public Table_6() : base()
+    {
+        _nextId++;
+        TableNumber = _nextId;
+    }
+}
