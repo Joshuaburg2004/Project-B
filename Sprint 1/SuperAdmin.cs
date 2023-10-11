17cc278a1ddccd30909ebed4a5bd850f62bc4ba6
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-10-11 10:43:53 +0200

    Update SuperAdmin.cs

--- Sprint 1/SuperAdmin.cs
+++ Sprint 1/SuperAdmin.cs
@@ -1,11 +1,13 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Data;
 using System.Net.Quic;
 using System.Text;
-public class SuperAdmin : Admin
+public class SuperAdmin
 {
-    public SuperAdmin(string name = "Gert-Jan den heijer", string email = "gjdenheijer@gmail.com", string password = "123", string role = "SuperAdmin") : base(name, email, password) { }
+    public static string Name = "Gert-Jan den heijer";
+    public static string Email = "gjdenheijer@gmail.com";
+    public static string Password = "123";
     public Admin AddAdmin(string Name, string Email, string Password)
     {
         Admin admin = Admin.CreateAdmin(Name, Email, Password);
