7b926f35f220d72c16694483dd721f3692dcc0d1
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-10-23 15:10:32 +0200

    Update Menu_List.cs

--- Sprint 1/Menu_List.cs
+++ Sprint 1/Menu_List.cs
@@ -8,27 +8,33 @@ public class Menu_List
             Console.WriteLine($"Name: {item.Name} | Category: {item.Category} | Price: {item.Price}");
         }
     }
-    public bool add_item()
-    {
-        Console.Write("Would you like to add an item to the Menu Y/N ?: ");
-        string choice = Console.ReadLine();
 
-        if(choice.ToUpper() == "Y")
+    public bool Add_item()
+    {
+        Console.WriteLine("What is the Name of the item ?: ");
+        string? name1 = Console.ReadLine();
+        Console.WriteLine("What is the Category (fish/meat/vegan/vegetarian) ?:");
+        string? category1 = Console.ReadLine();
+        Console.WriteLine("What will the price be ?:");
+        string? price1 = Console.ReadLine();
+        foreach (Menu menu in Menu_item)
         {
-            Console.WriteLine("What is the Name of the item ?: ");
-            string name1 = Console.ReadLine();
-            Console.WriteLine("What is the Category (fish/meat/vegan/vegetarian) ?:");
-            string category1 = Console.ReadLine();
-            Console.WriteLine("What will the price be ?:");
-            double price1 = Convert.ToDouble(Console.ReadLine());
-            foreach(Menu menu in Menu_item){
-                if(menu.Name == name1){
-                    return false;
-                }
+            if (menu.Name == name1)
+            {
+                return false;
             }
-            Menu_item.Add(new Menu(name1, category1, price1));
+        }
+        if(name1 is not null && category1 is not null && price1 is not null)
+        {
+            Menu_item.Add(new Menu(name1, category1, Convert.ToDouble(price1)));
             return true;
         }
+        return false;
+    }
+
+    public bool Remove_item()
+    {
+        //TODO
         return false;
     }
 }
