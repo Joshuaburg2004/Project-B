6a8366a36395c63d9fe4fb9d571ce73f2df12745
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-11-08 10:37:46 +0100

    Update Menu_List.cs

--- Sprint 1/Menu_List.cs
+++ Sprint 1/Menu_List.cs
@@ -8,7 +8,7 @@ public static class Menu_List
     public static List<Menu> Menu_item = new List<Menu> { };
     static Menu_List()
     {
-        string? FileCont = ControllerJson.ReadJson("file.json");
+        string? FileCont = ControllerJson.ReadJson("Menu.json");
         if (FileCont is not null)
         { 
             Menu_item = JsonConvert.DeserializeObject<List<Menu>>(FileCont) ?? new List<Menu> { }; 
