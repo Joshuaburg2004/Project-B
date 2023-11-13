4de1ebf4c7023b9f83be4d1ad66ca09dabc3c937
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-11-13 15:57:41 +0100

    Update AccountManager.cs

--- Sprint 1/AccountManager.cs
+++ Sprint 1/AccountManager.cs
@@ -4,18 +4,22 @@ using System.Data;
 using System.Net.Quic;
 using System.Security.Principal;
 using System.Text;
+// houd alle accounts bij.
 public static class AccountManager
 {
     public static List<Customer> Customers = new();
     public static List<Admin> Admins = new();
+    // houdt de superadmin bij
     public static SuperAdmin superAdmin = new();
     static AccountManager()
     {
+        // Vult Customers uit de json
         string? FileCont1 = ControllerJson.ReadJson("Customers.json");
         if (FileCont1 != null)
         {
             Customers = JsonConvert.DeserializeObject<List<Customer>>(FileCont1) ?? new List<Customer> { };
         }
+        // Vult Admins uit de json
         string? FileCont2 = ControllerJson.ReadJson("Admins.json");
         if (FileCont2 != null)
         {
