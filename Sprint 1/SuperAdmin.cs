900ed36ffa70456ba03767f5294605b32bb96087
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-11-13 16:21:40 +0100

    Update SuperAdmin.cs

--- Sprint 1/SuperAdmin.cs
+++ Sprint 1/SuperAdmin.cs
@@ -7,6 +7,7 @@ public class SuperAdmin : Account
 {
     // essentially static, SuperAdmin is always the same
     public SuperAdmin() : base("Gert-Jan den heijer", "gjdenheijer@gmail.com", "123", "SuperAdmin") { }
+    // voegt de admin toe
     public Admin AddAdmin(string Name, string Email, string Password)
     {
         Admin admin = Admin.CreateAdmin(Name, Email, Password);
