21063053cac47806b7909ae1f47e58bee6eeb6a4
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-11-13 16:04:57 +0100

    Update Menu.cs

--- Sprint 1/Menu.cs
+++ Sprint 1/Menu.cs
@@ -5,7 +5,7 @@
 
 public class Menu
 {
-    public static List<Menu> Menu_item = new(){new Menu("Lahmacun", "Meat", 6.99), new Menu("Pizza pepperoni","Meat",12.5)};
+    // public static List<Menu> Menu_item = new(){new Menu("Lahmacun", "Meat", 6.99), new Menu("Pizza pepperoni","Meat",12.5)};
     public double Price;
     public string Category;
     public string Name;
@@ -15,18 +15,18 @@ public class Menu
         this.Category = category;
         this.Name = name;
     }
-    public static List<Menu> view_menu()
-    {
-        Menu item1 = new Menu("Lahmacun","Meat",6.99);
-        Menu item2 = new Menu("Pizza pepperoni","Meat",12.5);
-        Menu_item.Add(item1);
-        Menu_item.Add(item2);
-        return Menu_item;
-    }
+    // public static List<Menu> view_menu()
+    // {
+    //     Menu item1 = new Menu("Lahmacun","Meat",6.99);
+    //     Menu item2 = new Menu("Pizza pepperoni","Meat",12.5);
+    //     Menu_item.Add(item1);
+    //     Menu_item.Add(item2);
+    //     return Menu_item;
+    // }
 
     public static void view()
     {
-        foreach(Menu item in Menu_item)
+        foreach(Menu item in Menu_List.Menu_item)
         {
             Console.WriteLine($"Name: {item.Name} Category: {item.Category} Price: {item.Price}");
         }
