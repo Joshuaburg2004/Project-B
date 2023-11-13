d7d935069c9a882826b93beb55b4184ca76cabd8
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-11-13 16:09:17 +0100

    Update Menu_List.cs

--- Sprint 1/Menu_List.cs
+++ Sprint 1/Menu_List.cs
@@ -5,9 +5,11 @@ using System.Net.Quic;
 using System.Text;
 public static class Menu_List
 {
+    // wordt gebruikt om alle menu items bij te houden
     public static List<Menu> Menu_item = new List<Menu> { };
     static Menu_List()
     {
+        // leest het menu uit de json
         string? FileCont = ControllerJson.ReadJson("Menu.json");
         if (FileCont is not null)
         { 
@@ -18,6 +20,7 @@ public static class Menu_List
             Menu_item = new List<Menu> { };
         }
     }
+    // Print alle items.
     public static void view()
     {
         foreach(Menu item in Menu_item)
@@ -25,7 +28,7 @@ public static class Menu_List
             Console.WriteLine($"Name: {item.Name} | Category: {item.Category} | Price: {item.Price}");
         }
     }
-
+    // Voegt een item toe aan de list, returned of het goed ging
     public static bool Add_item(string? name1, string? category1, string? price1)
     {
         foreach (Menu menu in Menu_item)
@@ -45,7 +48,7 @@ public static class Menu_List
         }
         return false;
     }
-
+    // haalt een item uit de list, returned of het goed ging
     public static bool Remove_item(string? name, string? category, string? price)
     {
         if (name is not null && category is not null && price is not null)
