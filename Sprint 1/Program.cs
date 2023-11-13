5bfa8596e4e37c122b80e3c493d706ace93a3ee2
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-11-13 09:58:29 +0100

    Update Program.cs

--- Sprint 1/Program.cs
+++ Sprint 1/Program.cs
@@ -49,7 +49,7 @@ public class Program
             }
             else if (curr_account as SuperAdmin is not null)
             {
-                Console.WriteLine("SuperAdmin logged in");
+                Console.WriteLine("SuperAdmin logged in");  // Placeholder, TODO: SuperAdminMenu()
             }
             if (input == "Q")
                 break;
