a7e05a30ac06cfd03ab0d17cf4ad675ae41251a9
Ber-kan <114362900+Ber-kan@users.noreply.github.com>
2023-11-22 09:28:08 +0100

    Update Table_6.cs

--- Sprint 1/tables/Table_6.cs
+++ Sprint 1/tables/Table_6.cs
@@ -1,1 +1,11 @@
+public class Table_4 : Table
+{
+    private static int _nextId = 2;
+    public int TableNumber;
 
+    public Table_4() : base()
+    {
+        _nextId++;
+        TableNumber = _nextId;
+    }
+}
