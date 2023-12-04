8af978caf02776342e7226446e4500fa1fe33de5
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-12-04 15:50:09 +0100

    Update CustomerTest.cs

--- Sprint 1/.tests/CustomerTest.cs
+++ Sprint 1/.tests/CustomerTest.cs
@@ -22,12 +22,28 @@ public class CustomerTest
         Customer customer = Customer.CreateAccount("a", "b", "c");
         Customer customer1 = Customer.CreateAccount("d", "e", "f");
         Customer customer2 = Customer.CreateAccount("g", "h", "i");
-        Assert.IsTrue(AccountManager.Customers.Contains(customer));
-        Assert.IsTrue(AccountManager.Customers.Contains(customer1));
-        Assert.IsTrue(AccountManager.Customers.Contains(customer2));
-        Assert.AreEqual(customer, Customer.GetCustomer(customer.Id));
-        Assert.AreEqual(customer1, Customer.GetCustomer(customer1.Id));
-        Assert.AreEqual(customer2, Customer.GetCustomer(customer2.Id));
+        Assert.IsTrue(Manager.Customers.Contains(customer));
+        Assert.IsTrue(Manager.Customers.Contains(customer1));
+        Assert.IsTrue(Manager.Customers.Contains(customer2));
+        Assert.AreEqual(customer, Customer.GetCustomerByID(customer.ID));
+        Assert.AreEqual(customer1, Customer.GetCustomerByID(customer1.ID));
+        Assert.AreEqual(customer2, Customer.GetCustomerByID(customer2.ID));
     }
-
+    [TestMethod]
+    public void TestLogIn()
+    {
+        Customer customer = Customer.CreateAccount("name", "email", "password");
+        Customer? customer1 = Customer.Log_in("name", "email", "password");
+        Assert.IsTrue(customer1!.Name == customer.Name);
+        Assert.IsTrue(customer1!.Email == customer.Email);
+        Assert.IsTrue(customer1!.Password == customer.Password);
+    }
+    [TestMethod]
+    public void TestAddReservation()
+    {
+        Customer customer = Customer.CreateAccount("name", "email", "password");
+        Reservation reservation = customer.Add_Reservation(2, 5, new DateOnly(year: 2023, month: 12, day: 12), "2")!;
+        Assert.IsTrue(customer.My_Reservation.Contains(reservation!));
+        Assert.IsTrue(Reservation.All_Reservations.Contains(reservation));
+    }
 }
