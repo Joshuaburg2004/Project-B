aa14b6876403688342dbcc0488a6c8209f11a99b
AlperenBaytimur <144028041+AlperenBaytimur@users.noreply.github.com>
2023-12-11 13:55:46 +0100

    Add files via upload

---
+++ Sprint 1/.tests/MenuTest.cs
@@ -0,0 +1,43 @@
+﻿namespace ProjectB.Tests
+{
+    [TestClass]
+    public class MenuTest
+    {
+        [TestMethod]
+        public void TestAddItem()
+        {
+            Menu_List.Menu_item = new();
+            // Arrange
+            string itemName = "TestItem";
+            string category = "TestCategory";
+            string price = "10.99";
+
+            // Act
+            bool added = Menu_List.Add_item(itemName, category, price);
+
+            // Assert
+            Assert.IsTrue(added);
+            // Add more assertions if needed
+        }
+
+        [TestMethod]
+        public void TestRemoveItem()
+        {
+            Menu_List.Menu_item = new();
+            // Arrange
+            string itemName = "TestItem";
+            string category = "TestCategory";
+            string price = "20.99";
+            Menu_List.Add_item(itemName, category, price);
+
+            // Act
+            bool removed = Menu_List.Remove_item(itemName, category, price);
+
+            // Assert
+            Assert.IsTrue(removed);
+            // Add more assertions if needed
+        }
+
+        // Add more test methods for other Menu related functionalities...
+    }
+}
---
+++ Sprint 1/.tests/ReservationTest.cs
@@ -0,0 +1,56 @@
+﻿namespace ProjectB.Tests;
+
+[TestClass]
+public class ReservationTest
+{
+    [TestMethod]
+    public void TestReservationCreation()
+    {
+        int customerId = 1;
+        int table = 3;
+        int guests = 2;
+        DateOnly date = new DateOnly(2023, 12, 15);
+        string time = "18:30";
+
+        Reservation reservation = new Reservation(customerId, table, guests, date, time);
+
+        Assert.AreEqual(customerId, reservation.CustomerId);
+        Assert.AreEqual(table, reservation.Table);
+        Assert.AreEqual(guests, reservation.Guests);
+        Assert.AreEqual(date, reservation.Date);
+        Assert.AreEqual(time, reservation.Time);
+    }
+
+    [TestMethod]
+    public void TestReservationInfo()
+    {
+        int customerId = 1;
+        int table = 3;
+        int guests = 2;
+        DateOnly date = new DateOnly(2023, 12, 15);
+        string time = "18:30";
+
+        Reservation reservation = new Reservation(customerId, table, guests, date, time);
+
+        string expectedInfo = $"CustomerID: {customerId}, Reservation_ID: {reservation.Reservation_ID}, Table {table}, number of guests: {guests}, Date: {date}, Time: {time}";
+
+        Assert.AreEqual(expectedInfo, reservation.Reservation_Info());
+    }
+
+    [TestMethod]
+    public void TestReservationInfoWithCustomer()
+    {
+        int customerId = 1;
+        int table = 3;
+        int guests = 2;
+        DateOnly date = new DateOnly(2023, 12, 15);
+        string time = "18:30";
+
+        Reservation reservation = new Reservation(customerId, table, guests, date, time);
+        Customer customer = new Customer("John Doe", "john@example.com", "password");
+
+        string expectedInfo = $"{Customer.Info(customer)}, Table: {table}, Guests: {guests}, Date: {date}, Time: {time}";
+
+        Assert.AreEqual(expectedInfo, Reservation.Info(reservation, customer));
+    }
+}
