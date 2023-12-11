6dbf6def9a5a8ea0e03b93317825798a657d12c5
AlperenBaytimur <144028041+AlperenBaytimur@users.noreply.github.com>
2023-12-11 13:06:07 +0100

    Update Menu.cs

--- Sprint 1/Menu/Menu.cs
+++ Sprint 1/Menu/Menu.cs
@@ -5,7 +5,7 @@
 //Gemaakt door Alperen en Berkan
 public class Menu
 {
-    // public static List<Menu> Menu_item = new(){new Menu("Lahmacun", "Meat", 6.99), new Menu("Pizza pepperoni","Meat",12.5)};
+    /*public static List<Menu> Menu_item = new() { new Menu("Lahmacun", "Meat", 6.99), new Menu("Pizza pepperoni", "Meat", 12.5) };*/
     public double Price;
     public string Category;
     public string Name;
@@ -16,67 +16,67 @@ public class Menu
         this.Category = category;
         this.Name = name;
     }
-    // public static List<Menu> view_menu()
-    // {
-    //     Menu item1 = new Menu("Lahmacun","Meat",6.99);
-    //     Menu item2 = new Menu("Pizza pepperoni","Meat",12.5);
-    //     Menu_item.Add(item1);
-    //     Menu_item.Add(item2);
-    //     return Menu_item;
-    // }
-    // Print alle menu_items.
-    
+    /*public static List<Menu> view_menu()
+    {
+        Menu item1 = new Menu("Lahmacun", "Meat", 6.99);
+        Menu item2 = new Menu("Pizza pepperoni", "Fish", 12.5);
+        Menu_item.Add(item1);
+        Menu_item.Add(item2);
+        return Menu_item;
+    }*/
     // Gemaakt door Alperen
     public static void view()
     {
-    Console.WriteLine("(1)Everything\n(2)Choose by Category");
-    string? ans = Console.ReadLine();
-    if(ans is not null)
-    {
-        if (ans == "1")
+        Console.WriteLine("(1)Everything\n(2)Choose by Category");
+        string? ans = Console.ReadLine();
+        if(ans is not null)
         {
-            foreach(Menu item in Menu_List.Menu_item)
+            if (ans == "1")
             {
-                Console.WriteLine($"|Name: {item.Name}|Category: {item.Category}|Price: {item.Price}|");
+                foreach(Menu item in Menu_List.Menu_item)
+                {
+                    Console.WriteLine($"|Name: {item.Name}|Category: {item.Category}|Price: {item.Price}|");
+                }
             }
-        }
-        else if(ans == "2")
-        {       
-            Console.WriteLine("(Fish/Meat/Vegan/Vegetarian)");
-            string? ans1 = Console.ReadLine();
-            foreach (Menu item in Menu_List.Menu_item)
-            {
-                if(ans1 == "Fish")
+            else if(ans == "2")
+            {       
+                Console.WriteLine("(Fish/Meat/Vegan/Vegetarian)");
+                string? ans1 = Console.ReadLine();
+                foreach (Menu item in Menu_List.Menu_item)
                 {
-                    if(item.Category == "Fish")
+                    if(ans1 == "Fish")
                     {
-                        Console.WriteLine($"|Name: {item.Name}|Category: {item.Category}|Price: {item.Price}|");
+                        if (item.Category == "Fish".ToUpper())
+                        {
+                            Console.WriteLine($"|Name: {item.Name}|Category: {item.Category}|Price: {item.Price}|");
+                        }
                     }
-                }
-                else if (ans1 == "Vegan")
-                {
-                    if (item.Category == "Vegan")
+                    else if (ans1 == "Vegan".ToUpper())
                     {
-                        Console.WriteLine($"|Name: {item.Name}|Category: {item.Category}|Price: {item.Price}|");
+                        if (item.Category == "Vegan")
+                        {
+                            Console.WriteLine($"|Name: {item.Name}|Category: {item.Category}|Price: {item.Price}|");
+                        }
                     }
-                }
-                else if (ans1 == "Vegetarian")
-                {
-                    if (item.Category == "Vegetarian")
+                    else if (ans1 == "Vegetarian".ToUpper())
                     {
-                        Console.WriteLine($"|Name: {item.Name}|Category: {item.Category}|Price: {item.Price}|");
+                        if (item.Category == "Vegetarian")
+                        {
+                            Console.WriteLine($"|Name: {item.Name}|Category: {item.Category}|Price: {item.Price}|");
+                        }
                     }
-                }
-                else if (ans1 == "Meat")
-                {
-                    if (item.Category == "Meat")
+                    else if (ans1 == "Meat")
                     {
-                        Console.WriteLine($"|Name: {item.Name}|Category: {item.Category}|Price: {item.Price}|");
+                        if (item.Category == "Meat".ToUpper())
+                        {
+                            Console.WriteLine($"|Name: {item.Name}|Category: {item.Category}|Price: {item.Price}|");
+                        }
                     }
-                }
 
+                }
             }
-        }
 
+        }
+        
     } 
 }
