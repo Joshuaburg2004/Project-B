d84bcdd2f81d82835eec5fe52ddd81d1e521fd2a
Ber-kan <114362900+Ber-kan@users.noreply.github.com>
2023-12-18 09:05:17 +0100

    Update Program.cs menu optie change reservation verandert naar change or cancel reservation

--- Sprint 1/Program.cs
+++ Sprint 1/Program.cs
@@ -542,7 +542,7 @@ public class Program
             Console.WriteLine("(1) View Menu");
             Console.WriteLine("(2) Reserve a table");
             Console.WriteLine("(3) View reservations");
-            Console.WriteLine("(4) Change reservation");
+            Console.WriteLine("(4) Change or cancel reservation");
             Console.WriteLine("(5) Log out");
             Console.WriteLine("(6) Close app");
             string? input = Console.ReadLine();
