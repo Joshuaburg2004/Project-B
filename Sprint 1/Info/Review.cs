6287662dc7c0b2bdbe11228435132e69e88d0efc
jerrari547 <144026455+jerrari547@users.noreply.github.com>
2024-01-09 11:55:54 +0100

    Create Review.cs

---
+++ Sprint 1/Info/Review.cs
@@ -0,0 +1,26 @@
+using Newtonsoft.Json;
+public class Review
+{
+    //gemaakt door sami
+    public int ReviewID { get; }
+    public int CustomerID { get; }
+    public string Text { get; }
+    public int Stars { get; }
+    public DateTime Date { get; }
+
+    public Review(int reviewID, int customerID, string text, int stars, DateTime date)
+    {
+        ReviewID = reviewID;
+        CustomerID = customerID;
+        Text = text;
+        Stars = stars;
+        Date = date;
+    }
+
+    public static List<Review> AllReviews { get; }
+    static Review()
+    {
+        string? FileCont = ControllerJson.ReadJson("Reviews.json");
+        AllReviews = JsonConvert.DeserializeObject<List<Review>>(FileCont) ?? new List<Review>{};
+    }
+}
