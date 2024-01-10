2ca5f233fb2527904a48354a8525c76b15b4ad0f
jerrari547 <144026455+jerrari547@users.noreply.github.com>
2024-01-10 13:49:33 +0100

    Update Info.cs (de telefoonnummer was int dus de 0 werdt niet weergegeven)

--- Sprint 1/Info/Info.cs
+++ Sprint 1/Info/Info.cs
@@ -1,13 +1,13 @@
 using System;
-
+//sami
 public class RestaurantInfo
 {
     public string Adres { get;  set; } = "Wijnhaven 107";
     public string Bereikbaarheid { get;  set; } = "Beurs, metrolijnen: A B C D E";
-    public int Telefoonnummer { get;  set; } = 064787943;
+    public string Telefoonnummer { get;  set; } = "064787943";
     public string Email { get;  set; } = "Gert_jan@hr.nl";
     public string Geschiedenis { get;  set; } = "Gert Jan was voorheen een docent, maar koos voor een nieuwe carrière als ondernemer. Hij houdt van lekker eten en heeft daarom dit restaurant geopend om zijn passie te delen.";
-    // print de info over het restaurant
+
     public void Info_Restaurant()
     {
         Console.WriteLine($"Adres: {Adres}");
