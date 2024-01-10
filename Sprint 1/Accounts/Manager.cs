8dac9dbb3bfa98b9153ad37de73e75150138d097
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2024-01-10 12:13:56 +0100

    edited manager a bit

--- Sprint 1/Accounts/Manager.cs
+++ Sprint 1/Accounts/Manager.cs
@@ -49,31 +49,30 @@ public static class Manager
 
     static Manager()
     {
-        List<Table>? Tables = new();
         Customers = ControllerJson.ReadJson<Customer>("Customers.json") ?? new List<Customer> { };
         Admins = ControllerJson.ReadJson<Admin>("Admins.json") ?? new List<Admin> { };
-        Tables = ControllerJson.ReadJson<Table>("Tables.json");
-        if(Tables is null || Tables.Count != 15)
+        table_list = ControllerJson.ReadJson<Table>("table_list.json") ?? new();
+        if(table_list is null || table_list.Count != 15)
         {
-            Tables = new List<Table>() { table_1, table_2, table_3, table_4, table_5, table_6, table_7, table_8, table_9, table_10, table_11, table_12, table_13, table_14, table_15 };
+            table_list = new List<Table>() { table_1, table_2, table_3, table_4, table_5, table_6, table_7, table_8, table_9, table_10, table_11, table_12, table_13, table_14, table_15 };
         }
         else
         {
-            table_1 = (Table_6)Tables[0];
-            table_2 = (Table_6)Tables[1];
-            table_3 = (Table_4)Tables[2];
-            table_4 = (Table_4)Tables[3];
-            table_5 = (Table_4)Tables[4];
-            table_6 = (Table_4)Tables[5];
-            table_7 = (Table_4)Tables[6];
-            table_8 = (Table_2)Tables[7];
-            table_9 = (Table_2)Tables[8];
-            table_10 = (Table_2)Tables[9];
-            table_11 = (Table_2)Tables[10];
-            table_12 = (Table_2)Tables[11];
-            table_13 = (Table_2)Tables[12];
-            table_14 = (Table_2)Tables[13];
-            table_15 = (Table_2)Tables[14];
+            table_1 = (Table_6)table_list[0];
+            table_2 = (Table_6)table_list[1];
+            table_3 = (Table_4)table_list[2];
+            table_4 = (Table_4)table_list[3];
+            table_5 = (Table_4)table_list[4];
+            table_6 = (Table_4)table_list[5];
+            table_7 = (Table_4)table_list[6];
+            table_8 = (Table_2)table_list[7];
+            table_9 = (Table_2)table_list[8];
+            table_10 = (Table_2)table_list[9];
+            table_11 = (Table_2)table_list[10];
+            table_12 = (Table_2)table_list[11];
+            table_13 = (Table_2)table_list[12];
+            table_14 = (Table_2)table_list[13];
+            table_15 = (Table_2)table_list[14];
         }
     }
 }
