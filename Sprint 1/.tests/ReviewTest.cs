fb9814427257f795a8544b6e35de322466d5b585
jerrari547 <144026455+jerrari547@users.noreply.github.com>
2024-01-11 19:36:05 +0100

    Create ReviewTest.cs ( 3 methodes voor tests

---
+++ Sprint 1/.tests/ReviewTest.cs
@@ -0,0 +1,42 @@
+namespace ProjectB.Tests;
+
+[TestClass]
+public class ReviewTests
+
+  {
+      [TestMethod]
+      public void TestLeaveReview()
+      {
+          int customerID = 1;
+          string reviewText = "nice restaurant";
+          int stars = 5;
+
+          Review.AllReviews.Clear();
+          //om accurate ding te hebben
+          Review.LeaveReview(customerID);
+
+          Assert.AreEqual(1, Review.AllReviews.Count);
+          Assert.AreEqual(customerID, Review.AllReviews[0].CustomerID);
+          Assert.AreEqual(reviewText, Review.AllReviews[0].Text);
+          Assert.AreEqual(stars, Review.AllReviews[0].Stars);
+      }
+
+      [TestMethod]
+      public void TestGetTotalStarsEmptyReviews()
+      {
+          Review.AllReviews.Clear();
+          double totalStars = Review.Get_Total_Stars();
+          Assert.AreEqual(0, totalStars);
+      }
+
+      [TestMethod]
+      public void TestLeaveReviewInvalidStars()
+      {
+          Review.AllReviews.Clear();
+          Review.LeaveReview(1, "test review bla bla", 6);
+          Assert.AreEqual(0, Review.AllReviews.Count);
+      }
+  }
+
+  
+
