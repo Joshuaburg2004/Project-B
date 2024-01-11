1b0d7b3c690a96e4942549dc476485bd04597fb7
jerrari547 <144026455+jerrari547@users.noreply.github.com>
2024-01-11 22:13:33 +0100

    Update AdminTest.cs ( foute input test)

--- Sprint 1/.tests/AdminTest.cs
+++ Sprint 1/.tests/AdminTest.cs
@@ -1,5 +1,5 @@
 namespace ProjectB.Tests;
-
+//sami
 [TestClass]
 public class AdminTests
 {
@@ -21,7 +21,18 @@ public class AdminTests
         Assert.AreEqual(email,Manager.Admins[0].Email);
         Assert.AreEqual(password,Manager.Admins[0].Password);
 
+    }
 
+    public void TestWrongInputs()
+    {
+        int name = 1;
+        int email = 2;
+        int password = 3;
+
+        Manager.Admins.Clear();
+        Admin.CreateAdmin(name,email,password);
+
+        Assert.AreEqual(0,Manager.Admins.Count);
     }
     
 }
