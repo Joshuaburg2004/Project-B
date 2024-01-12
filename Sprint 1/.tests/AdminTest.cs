4197bfd1a6004e8b18e484a67e9f75c7ca5ddd6e
jerrari547 <144026455+jerrari547@users.noreply.github.com>
2024-01-12 21:02:47 +0100

    Update AdminTest.cs

--- Sprint 1/.tests/AdminTest.cs
+++ Sprint 1/.tests/AdminTest.cs
@@ -22,19 +22,6 @@ public class AdminTests
         Assert.AreEqual(password,Manager.Admins[0].Password);
 
     }
-
-    public void TestWrongInputs()
-    {
-        int name = 1;
-        int email = 2;
-        int password = 3;
-
-        Manager.Admins.Clear();
-        Admin.CreateAdmin(name,email,password);
-
-        Assert.AreEqual(0,Manager.Admins.Count);
-    }
-
     public void check_if_password_restriction()
     {
         string name = "Bob";
